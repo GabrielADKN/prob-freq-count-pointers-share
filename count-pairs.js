25b9d2e9c3336be61c329f9a7334e49c7391abf2
gabrieladkn <adoukonoubienheureux@gmail.com>
2024-05-13 00:03:24 -0400

    adding count pair functions

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, sum) {
+    let count = 0;
+    let seen = new Set();
+
+    for (let num of arr) {
+        let diff = sum - num;
+        if (seen.has(diff)) {
+            count++;
+        }
+        seen.add(num);
+    }
+    return count;
 }
+
+
+module.exports = countPairs
